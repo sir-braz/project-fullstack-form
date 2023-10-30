3033e3145c9229f85370c51c327f94ed4e539dea
sir-braz <mrbrazdev@gmail.com>
2023-10-30 15:48:46 -0300

    Added UserService implementation

--- api/src/main/java/com/backend/api/service/userServiceImpl.java
+++ api/src/main/java/com/backend/api/service/userServiceImpl.java
@@ -1,4 +1,33 @@
 package com.backend.api.service;
 
-public class userServiceImpl {
+import com.backend.api.entity.User;
+
+import java.util.List;
+
+public class userServiceImpl implements UserService {
+
+    @Override
+    public void save(User user) {
+
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public User getUserById(Long userId) {
+        return null;
+    }
+
+    @Override
+    public void deleteUserById(Long userId) {
+
+    }
+
+    @Override
+    public void updateUser(User user, Long userId) {
+
+    }
 }
