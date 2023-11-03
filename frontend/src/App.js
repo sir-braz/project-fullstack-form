77193ed07f996ba94f7dd0106c3cfbeb812a1a1c
sir-braz <mrbrazdev@gmail.com>
2023-11-02 23:05:56 -0300

    Add user creation functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,69 @@
+import axios from 'axios';
+import React, { useState } from 'react';
 import './App.css';
 
+const baseURL = "http://localhost:8080/api/register"; 
+
+
 function App() {
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+    text: '',
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(baseURL, user);
+      console.log('POST response:', response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <form>
-      <div class='form-row'>
-        <label for="inputEmail4">Email</label>
-        <input type='email' class='form-control' id='inputEmail4' placeholder='Email' />
+      <div className='form-row'>
+        <label htmlFor="inputEmail4">Email</label>
+        <input type='email' className='form-control' id='inputEmail4' placeholder='Email' />
       </div>
-      <div class="form-group col-md-6">
-        <label for="inputPassword4">Password</label>
-        <input type='password' class="form-control" id='inputPassword' placeholder='Password' />
+      <div className="form-group col-md-6">
+        <label htmlFor="inputPassword4">Password</label>
+        <input type='password' className="form-control" id='inputPassword4' placeholder='Password' />
       </div>
-      <div class="form-group">
-          <label for="inputAddress">Address</label>
-          <input type='text' class="form-control" id='inputAddress' placeholder='1234 Main St'></input>
+      <div className="form-group">
+        <label htmlFor="inputAddress">Address</label>
+        <input type='text' className="form-control" id='inputAddress' placeholder='1234 Main St' />
       </div>
-      <div class='form-group'>
-        <label for="inputAddress2">Address 2</label>
-        <input type='text' class="form-control" id="iputAddress2" placeholder='Apartment, studio, or floor'></input>
+      <div className='form-group'>
+        <label htmlFor="inputAddress2">Address 2</label>
+        <input type='text' className="form-control" id="inputAddress2" placeholder='Apartment, studio, or floor' />
       </div>
-      <div class="form-row">
-        <div class="form-group col-md-6">
-          <label for="inputCity">City</label>
-          <input type='text' class="form-control" id='inputCity'></input>
+      <div className="form-row">
+        <div className="form-group col-md-6">
+          <label htmlFor="inputCity">City</label>
+          <input type='text' className="form-control" id='inputCity' />
+        </div>
+        <div className="form-group col-md-4">
+          <label htmlFor="inputState">State</label>
+          <select id='inputState' className="form-control">
+            <option>Choose...</option>
+            <option>...</option>
+          </select>
         </div>
       </div>
-      <div class="form-group col-md-4">
-        <label for="inputState">State</label>
-        <select id='inputState' class="form-control">
-          <option>Choose...</option>
-          <option>...</option>
-        </select>
-      </div>
-      <div class="form-group">
-        <div class="form-check">
-          <input class="form-check-input" type='checkbox' id='gridCheck'></input>
-          <label class="form-check-label" for="gridCheck">
+      <div className="form-group">
+        <div className="form-check">
+          <input className="form-check-input" type='checkbox' id='gridCheck' />
+          <label className="form-check-label" htmlFor="gridCheck">
             Check Me
           </label>
         </div>
       </div>
-      <button type='submit' class="btn btn-primary">Sign in</button>
+      <button type='submit' className="btn btn-primary" onClick={handleSubmit}>
+        Sign in
+      </button>
     </form>
   );
 }
